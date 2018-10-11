5dec7137a625e6c24f298717942c0a6eacf5c43b
nearizpe <nearizpe@gmail.com>
2018-10-11 17:15:12 -0500

    Added Sound to menu Buttons
    i also aadded a few more sound packs

---
+++ KyootieKillers/Assets/MenuClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuClick : MonoBehaviour {
+    public AudioSource audioData;
+    public AudioClip mySound;
+    // Use this for initialization
+    void Start () {
+        audioData = GetComponent<AudioSource>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        audioData.PlayOneShot(mySound);
+    }
+}
