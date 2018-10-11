842b34c21d38d1600c3938c14948f530be61aca2
yadiraH <yadira.hernandez021@yahoo.com>
2018-10-11 12:13:14 -0500

    the mob and partially implemented AI Movement

---
+++ KyootieKillers/Assets/UnicornAIMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnicornAIMovement : MonoBehaviour {
+
+    public Transform target;
+    UnityEngine.AI.NavMeshAgent agent;
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>(); // the agent component of
+    }
+    void Update()
+    {
+        agent.SetDestination(target.position); // move towards the target while avoiding things
+   }
+}
