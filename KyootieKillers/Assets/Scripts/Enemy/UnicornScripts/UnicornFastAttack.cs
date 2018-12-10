e9cef9d3d8d3c01eb948430a95505ca30efe2b99
yadiraH <yadira.hernandez021@yahoo.com>
2018-12-10 16:24:46 -0600

    issues trying to resolve

--- KyootieKillers/Assets/Scripts/Enemy/UnicornScripts/UnicornFastAttack.cs
+++ KyootieKillers/Assets/Scripts/Enemy/UnicornScripts/UnicornFastAttack.cs
@@ -3,52 +3,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnicornFastAttack : MonoBehaviour {
-    
+public class UnicornFastAttack : MonoBehaviour
+{
+
     public float velocity = 30f;
     public float attackRange = 10f;
     public float cooldown = 4f;
     public bool isAttacking = false;
     public GameObject player;
-    
+
     private Animator animator;
     private UnityEngine.AI.NavMeshAgent agent;
     private Rigidbody rBody;
     private Health HP;
     private float baseSpeed;
     private bool alreadyInvoked = false;
-    bool alive = true;
 
-
-
-    void Start () {
+    void Start()
+    {
         animator = GetComponent<Animator>();
         player = GameObject.FindWithTag("Player");
-
         rBody = GetComponent<Rigidbody>();
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         HP = GetComponent<Health>();
         baseSpeed = agent.speed;
-
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         CheckDead();
         transform.LookAt(player.transform);
         float dist = Vector3.Distance(transform.position, player.transform.position);
         // if( dist < agent.stoppingDistance){
-            if (!isAttacking){
-                HornAttack();
+        if (!isAttacking)
+        {
+            HornAttack();
             // }
-        } else {
+        }
+        else
+        {
             LookForPlayer();
         }
     }
-    
-    private void LookForPlayer(){
+
+    private void LookForPlayer()
+    {
         agent.speed = baseSpeed;
-        
+
         agent.SetDestination(player.transform.position);
         animator.SetInteger("animation", 5);
     }
@@ -57,14 +59,15 @@ public class UnicornFastAttack : MonoBehaviour {
     {
         agent.speed = velocity;
         isAttacking = true;
-        if (!alreadyInvoked){
+        if (!alreadyInvoked)
+        {
             alreadyInvoked = true;
             Invoke("clearIsAttacking", cooldown);
         }
         // rBody.AddForce(0, 0, 50, ForceMode.VelocityChange);
         // rBody.transform.Translate(player.transform.forward * velocity * Time.deltaTime);
         // if(originalPosition= null){
-            // originalPosition = rBody.transform;
+        // originalPosition = rBody.transform;
         // }
 
         //PLAY AUDIO SOURCE
@@ -74,9 +77,9 @@ public class UnicornFastAttack : MonoBehaviour {
         animator.SetInteger("animation", 7);
         //rBody.MovePosition(player.transform.position * speed);
         //rBody.transform.position = Vector3.Lerp(rBody.transform, player.transform.position, i);
-       // rBody.velocity = player.transform.forward * speed;
+        // rBody.velocity = player.transform.forward * speed;
 
-      
+
         //if (rBody.transform.position != player.transform.position)
         //{
         //   // isAttacking = true;
@@ -86,66 +89,64 @@ public class UnicornFastAttack : MonoBehaviour {
         //    playerHit = true;
         //    Debug.Log("Player is hit with fast attack");
         //}
-       // if(playerHit){
-            // Invoke("GoBackToOrginalPosition", 1f);
-      //  }
-   
+        // if(playerHit){
+        // Invoke("GoBackToOrginalPosition", 1f);
+        //  }
+
     }
-    
-    private void clearIsAttacking(){
+
+    private void clearIsAttacking()
+    {
         isAttacking = false;
         alreadyInvoked = false;
     }
     // private void GoBackToOrginalPosition()
     // {
-        // isAttacking = false;
-        // rBody.velocity = player.transform.position * (-1) * speed;
-        // rBody.transform.Translate(originalPosition.position*1000);
-        //rBody.velocity = new Vector3(0, 0, 0);
-        //rBody.velocity = Vector3.zero;
+    // isAttacking = false;
+    // rBody.velocity = player.transform.position * (-1) * speed;
+    // rBody.transform.Translate(originalPosition.position*1000);
+    //rBody.velocity = new Vector3(0, 0, 0);
+    //rBody.velocity = Vector3.zero;
 
-       // rBody.MovePosition(originalPosition.position + transform.forward * Time.deltaTime);
-      //  Debug.Log("Go back");
-        // transform.Translate(-transform.forward * speed * Time.deltaTime);
+    // rBody.MovePosition(originalPosition.position + transform.forward * Time.deltaTime);
+    //  Debug.Log("Go back");
+    // transform.Translate(-transform.forward * speed * Time.deltaTime);
     // }
-    
+
     // private void OnCollisionEnter(Collision collision)
     // {
-        //Decrement Health
-        // Debug.Log("Decrement Health");
+    //Decrement Health
+    // Debug.Log("Decrement Health");
     // }
-    
+
     void Die()
     {
-        
         agent.Stop();
         agent.SetDestination(transform.position);
         animator.SetInteger("animation", 10);
 
-        if(AnimationIsPlaying("Die") == false)
+        if (AnimationIsPlaying("Die") == false)
         {
             StartCoroutine(WaitForAnimation());
         }
     }
-    
+
     bool AnimationIsPlaying(string animation)
     {
         return animator.GetCurrentAnimatorStateInfo(0).IsName(animation);
     }
 
-
     private IEnumerator WaitForAnimation()
     {
         yield return new WaitForSeconds(3);
-        GameObject gen = GameObject.FindGameObjectWithTag("GameController");
-        gen.GetComponent<MobGenerator>().enemynum--;
         Destroy(gameObject);
     }
-    
-    private void CheckDead(){
-        if (HP.GetHealth() <= 0){
-            Die();
 
+    private void CheckDead()
+    {
+        if (HP.GetHealth() <= 0)
+        {
+            Die();
         }
     }
 }
